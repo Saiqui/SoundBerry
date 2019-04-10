a1de4cb84a085364695f33ac5b3dec39524b7f98
Luc Beaufils <luc.beaufils.2122@gmail.com>
2019-04-10 12:12:56 +0200

    ajout de commentaire explicatif

--- src/bluetooth_phone_control.py
+++ src/bluetooth_phone_control.py
@@ -9,6 +9,7 @@ valeurCommand = ""
 
 
 
+# on créer leserver bluetooth en rfcomm pour la lisaison SPP
 server = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 print('Socket bluetooth on')
 try:
@@ -21,16 +22,19 @@ client, adress = server.accept()
 print("Connecte a : ", adress)
 print("Client : ", client)
 
-# On prend le nom de l'interface wifi pour la portabilite
+
+# On prend le nom de l'interface wifi pour la portabilite du script car le nom change selon les versions/devices
 wifiInterface = os.popen("ip -o a | grep \"wl\" | head -n1 | awk '{print $2}'").read()
 
 
+#fonction qui arrête la connection puis relance le script automatiquement
 def end_connection():
 	client.close()
 	server.close()
 	os.system("python /home/pi/SoundBerry/src/bluetooth_phone_control.py")
 	
-	
+
+#fonction qui analyse la trame reçut par bluetooth :       keyCommand.valeurCommand
 def analyse_trame(donnee):
 		global keyCommand
 		global valeurCommand
@@ -44,6 +48,8 @@ def analyse_trame(donnee):
 				keyCommand = donnee[i-1]
 				valeurCommand = donnee[position_point+1 : len(donnee)]
 
+
+# reset de keyCommand et ValeurCommand pour eviter des problèmes
 def reset_command():
 	global keyCommand
 	global valeurCommand
@@ -51,6 +57,8 @@ def reset_command():
 	keyCommand = 	""
 	valeurCommand = ""
 
+
+# fonction qui permet de recuperer la ssid et le pass du wifi reçu par trame :			ssid.password
 def wifiPswdDecodage(trame):
 	global wifiSSID
 	global wifiPass
@@ -65,6 +73,7 @@ def wifiPswdDecodage(trame):
 			print(wifiSSID)
 		
 	
+# fonction qui definit chaque action possible grâce a keyCommand et valeurCommand
 def case_keyCommand(key, commandKey):
 	if (key == 'v' and commandKey != 'u' and commandKey != 'm'):
 		commande = "amixer set Master " + valeurCommand
@@ -80,35 +89,17 @@ def case_keyCommand(key, commandKey):
 	if key == 'm':
 		os.system("amixer sset Master toggle")
 	if key == '0':
-		commande = "amixer -D equal sset 00.\ 31\ Hz " + valeurCommand
+		commande = "amixer -D equal sset 00.\ 31\ Hz " + valeurCommand + " && amixer -D equal sset 01.\ 63\ Hz " + valeurCommand + " && amixer -D equal sset 02.\ 125\ Hz " + valeurCommand
 		os.system(commande)
+
 	if key == '1':
-		commande = "amixer -D equal sset 01.\ 63\ Hz " + valeurCommand
+		commande = "amixer -D equal sset 03.\ 250\ Hz " + valeurCommand + "&& amixer -D equal sset 04.\ 500\ Hz " + valeurCommand + " && amixer -D equal sset 05.\ 1\ kHz " + valeurCommand + " && amixer -D equal sset 06.\ 2\ kHz " + valeurCommand
 		os.system(commande)
+
 	if key == '2':
-		commande = "amixer -D equal sset 02.\ 125\ Hz " + valeurCommand
+		commande = "amixer -D equal sset 07.\ 4\ kHz " + valeurCommand + " && amixer -D equal sset 08.\ 8\ kHz " + valeurCommand + " && amixer -D equal sset 09.\ 16\ kHz " + valeurCommand
 		os.system(commande)
-	if key == '3':
-		commande = "amixer -D equal sset 03.\ 250\ Hz " + valeurCommand
-		os.system(commande)
-	if key == '4':
-		commande = "amixer -D equal sset 04.\ 500\ Hz " + valeurCommand
-		os.system(commande)
-	if key == '5':
-		commande = "amixer -D equal sset 05.\ 1\ kHz " + valeurCommand
-		os.system(commande)
-	if key == '6':
-		commande = "amixer -D equal sset 06.\ 2\ kHz " + valeurCommand
-		os.system(commande)
-	if key == '7':
-		commande = "amixer -D equal sset 07.\ 4\ kHz " + valeurCommand
-		os.system(commande)
-	if key == '8':
-		commande = "amixer -D equal sset 08.\ 8\ kHz " + valeurCommand
-		os.system(commande)
-	if key == '9':
-		commande = "amixer -D equal sset 09.\ 16\ kHz " + valeurCommand
-		os.system(commande)
+
 		
 	if key == 'w' and commandKey == 'l':
 		commande = "sudo iwlist " + wifiInterface.rstrip() + " scan | grep -i 'essid' >> wifi.txt"
@@ -117,6 +108,7 @@ def case_keyCommand(key, commandKey):
 		essid = open("wifi.txt", "r")
 		data = []
 		k=0
+
 		while 1:
 			line=essid.readline()
 			if not line: break
@@ -129,7 +121,8 @@ def case_keyCommand(key, commandKey):
 				i += 1
 			data.append(":")
 			k+=1
-
+		
+		
 		data_send = "".join(data)
 		print(data_send)
 
@@ -145,12 +138,23 @@ def case_keyCommand(key, commandKey):
 		data = client.recv(1024)
 		print('donnee recu')
 		wifiPswdDecodage(data)
-		data = ""
+		commande = "sudo nmcli d wifi connect \"" + wifiSSID + "\" password \"" + wifiPass + "\""
+		os.system(commande)
+		
+	if key == 'i':
+		
+		info_ssid = "nmcli connection show | grep -i 'wlan0' | awk '{print$1}'"
+		os.system(info_ssid)
+		info_ip = "hostname -I | awk '{print $1}'"
+		os.system(info_ip)
+		info = os.popen(info_ssid).read().rstrip() + ':' + os.popen(info_ip).read().rstrip() + ':'
+		print(info)
+		client.send(info)
 		
 	if key == 'c':
 		end_connection()
 		
-
+# coeur du programme qui tourne en boucle jusqu'a la fin de la connexion bluetooth
 try:
 	while True:
 		
@@ -162,7 +166,8 @@ try:
 		print(valeurCommand)
 		case_keyCommand(keyCommand, valeurCommand)
 		reset_command()
-		print('====================================================================')
+		print('====================================================================\n')
 
 except:
 	end_connection()
+	
